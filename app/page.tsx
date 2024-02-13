8ba3c055c9f8b8fe0aa337dec748800b7fc8b66a
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-02-13 12:24:30 +0100

    initialized

---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button>Click me</Button>
+      <Image
+        src="/vercel.svg"
+        alt="Vercel Logo"
+        className="dark:invert"
+        width={100}
+        height={24}
+        priority
+      />
+    </div>
+  );
+};
+
+export default Home;
