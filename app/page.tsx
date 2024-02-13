48998c90cd30e716e07971ae8279a340e0eaeada
Oluwapelumi Israel <oluwapelumisotoyinbo@gmail.com>
2024-02-13 13:24:53 +0100

    fix: checks

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from '@/components/ui/button';
 const Home = () => {
   return (
     <div>
-      <Button>Click me</Button>
+      <Button>I'm hungry</Button>
       <Image
         src="/vercel.svg"
         alt="Vercel Logo"
@@ -18,48 +18,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// name: gasfleet-client checks
-
-// on:
-//   push:a
-//     branches: [dev, pro]
-//   pull_request:
-//     branches: [dev, pro]
-
-// jobs:
-//   install:
-//     runs-on: ubuntu-latest
-//     steps:
-//       - name: View context attributes
-//         uses: actions/github-script@v6
-//         with:
-//           script: |
-//             console.log(context);
-
-//   prettier:
-//     runs-on: ubuntu-latest
-//     needs: install
-//     steps:
-//       - uses: actions/checkout@v3
-//       - run: npm install
-//       - run: npm run prettier:check
-
-//   typescript:
-//     runs-on: ubuntu-latest
-//     needs: install
-//     steps:
-//       - uses: actions/checkout@v3
-//       - run: npm install
-//       - run: npm run typescript
-
-//   build:
-//     runs-on: ubuntu-latest
-//     needs: install
-//     env:
-//       NEXT_PUBLIC_SANITY_PROJECT_ID: k68zk0vr
-//       NEXT_PUBLIC_SANITY_DATASET: production
-//     steps:
-//       - uses: actions/checkout@v3
-//       - run: npm install
-//       - run: npm run build
